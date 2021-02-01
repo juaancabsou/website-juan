f20673d661d19df03e5fcb7e3aa481ff18dd3950
Juan <juaancabsou@gmail.com>
2021-02-01 19:35:23 +0100

    [website-juan] Adding sections. I should add also a route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarMenuComponent
+    NavbarMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
